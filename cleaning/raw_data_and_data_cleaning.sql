5f894ac29d848f33acd5600de69943664b86d119
Pabo-0 <134642717+Pabo-0@users.noreply.github.com>
2024-12-01 16:38:18 -0600

    Solución de errores en la creación e inserción de los datos limpios

--- cleaning/raw_data_and_data_cleaning.sql
+++ cleaning/raw_data_and_data_cleaning.sql
@@ -1,63 +1,93 @@
--- Cambio de prueba jeje
+-- Se requiere de la extensión PostGIS para poder guardar datos de tipo POINT como coordenadas
+-- el proceso de su isntalación está especificado en raw_data_schema_creation.sql
 
+
+
+-- .: CREACIÓN DE LOS ESQUEMAS :.
+
+--Creación del esquema para la limpieza de los datos
 DROP SCHEMA IF EXISTS cleaning CASCADE;
 CREATE SCHEMA cleaning;
 
---Creación de tablas de limpieza
+-- Creación de la tabla para los servicios de energía
 DROP TABLE IF EXISTS cleaning.electric_utility;
 CREATE TABLE cleaning.electric_utility (
-                                           id SERIAL PRIMARY KEY,
-                                           name text
+    id SERIAL PRIMARY KEY, -- Es serial pues no esperamos una gran cantidad de servicios
+    name TEXT
 );
 
+-- Creación de la tabla para la ubicación del vehículo
 DROP TABLE IF EXISTS cleaning.location;
 CREATE TABLE cleaning.location (
-                                    id serial primary key,
-                                   postal_code varchar(10),
-                                   county varchar(50),
-                                   state varchar(50),
-                                   city varchar(50),
-                                   vehicle_location text,
-                                   legislative_district varchar(4)
+    id BIGSERIAL PRIMARY KEY,
+    postal_code VARCHAR(10),
+    country VARCHAR(50),
+    state VARCHAR(2),
+    city VARCHAR(50),
+    legislative_district VARCHAR(2),
+    vehicle_location GEOMETRY(Point, 4326)
 );
 
+-- Creación de la tabla para los detalles del vehículo
 DROP TABLE IF EXISTS cleaning.vehicle_details;
 CREATE TABLE cleaning.vehicle_details (
-                                          id bigserial primary key ,
-                                          model varchar(50),
-                                          make varchar(50),
-                                          model_year smallint,
-                                          vehicle_type varchar(50),
-                                          range smallint,
-                                          baseMSRP numeric(10,2),
-                                          CAFV varchar(100)
+    id BIGSERIAl PRIMARY KEY,
+    model VARCHAR(50),
+    make VARCHAR(50),
+    model_year SMALLINT,
+    vehicle_type VARCHAR(100),
+    range SMALLINT,
+    baseMSRP NUMERIC(10,2),
+    CAFV VARCHAR(100)
 );
 
+-- Creación de la tabla general para los vehículos
 DROP TABLE IF EXISTS cleaning.vehicle;
 CREATE TABLE cleaning.vehicle (
     dol_vehicle_id BIGINT PRIMARY KEY,
-    vehicle_details_id BIGINT references cleaning.vehicle_details(id),
-    location_id BIGINT references cleaning.location(id),
-    electric_utility_id BIGINT references cleaning.electric_utility(id)
+    vehicle_details_id BIGSERIAL references cleaning.vehicle_details(id),
+    location_id BIGSERIAL references cleaning.location(id),
+    electric_utility_id SERIAL references cleaning.electric_utility(id)
 );
 
 
---Primera etapa de limpieza: limpieza general y orden
-INSERT INTO cleaning.vehicle_details (model, make, model_year, vehicle_type, range, baseMSRP, CAFV)
-SELECT DISTINCT trim(model), trim(make), model_year, trim(upper(vehicle_type)), range, baseMSRP, trim(upper(CAFV))
-FROM raw.vehicle_data;
 
+--  .: INSERCIÓN DE LOS DATOS LIMPIOS :.
 
+-- Inserción de los datos del servicio de energía
 INSERT INTO cleaning.electric_utility (name)
-SELECT DISTINCT trim(electric_utility)
+SELECT DISTINCT electric_utility
 FROM raw.vehicle_data;
 
+-- Comprobar que los datos se agregaron de manera correcta (75 tuplas)
+SELECT * FROM cleaning.electric_utility;
 
-INSERT INTO cleaning.location (postal_code, county, state, city, vehicle_location, legislative_district)
-SELECT DISTINCT postal_code, trim(upper(county)), trim(state), trim(upper(city)), vehicle_location, legislative_district
+
+-- Inserción de los datos de la ubicación del vehìculo
+INSERT INTO cleaning.location (postal_code, country, state, city, vehicle_location, legislative_district)
+SELECT DISTINCT postal_code,country, state, city, vehicle_location, legislative_district
 FROM raw.vehicle_data;
 
+-- Comprobar que los datos se agregaron de manera correcta (1,438 tuplas)
+SELECT * FROM cleaning.location;
 
+
+-- Inserción de los datos de la información acerca del vehìculo
+INSERT INTO cleaning.vehicle_details (model, make, model_year, vehicle_type, range, baseMSRP, CAFV)
+SELECT DISTINCT model, make, model_year, vehicle_type, range, baseMSRP, CAFV
+FROM raw.vehicle_data;
+
+-- Comprobar que los datos se agregaron de manera correcta (596 tuplas)
+SELECT * FROM cleaning.vehicle_details;
+
+-- Inserción de los datos de todas las llaves para la tabla de vehículo
+-- NOTAS PARA ENTENDER LOS LEFT JOINS:
+-- Existen 3 tuplas que no tienen registrado un servicio de energía
+-- Un mismo codigo postal puede tener distintos distritos legislativosm, ciudades o condados
+-- Hay tuplas que solo tien estado y ninguna otra información
+-- Hay tuplas que no tienen información de ubicación
+-- Existe 1 tuplas que no tienen registrado un servicio de energía
+-- Puede que un vehículo de mismo modelo y año tenga un precio sugerido, autonomía y tipo de vehículo diferente
 INSERT INTO cleaning.vehicle (dol_vehicle_id, vehicle_details_id, location_id, electric_utility_id)
 SELECT DISTINCT
     rv.dol_vehicle_id,
@@ -65,19 +95,19 @@ SELECT DISTINCT
     loc.id AS location_id,
     eu.id AS electric_utility_id
 FROM raw.vehicle_data rv
-         LEFT JOIN cleaning.vehicle_details vd
-                   ON TRIM(rv.model) = vd.model
-                       AND TRIM(rv.make) = vd.make
-                       AND rv.model_year = vd.model_year
-         LEFT JOIN cleaning.location loc
-                   ON rv.postal_code = loc.postal_code
-         LEFT JOIN cleaning.electric_utility eu
-                   ON TRIM(rv.electric_utility) = eu.name;
+LEFT JOIN cleaning.electric_utility eu
+    ON rv.electric_utility = eu.name
+LEFT JOIN cleaning.location loc
+    ON rv.postal_code = loc.postal_code
+    AND rv.city = loc.city
+    AND rv.legislative_district = loc.legislative_district
+    AND rv.country = loc.country
+LEFT JOIN cleaning.vehicle_details vd
+    ON rv.model = vd.model
+    AND rv.model_year = vd.model_year
+    AND rv.range = vd.range
+    AND rv.basemsrp = vd.baseMSRP
+    AND rv.vehicle_type = vd.vehicle_type;
 
-
-insert into cleaning.vehicle (dol_vehicle_id)
-SELECT DISTINCT dol_vehicle_id
-FROM raw.vehicle_data;
-
-
---DROP TABLE raw.vehicle_data;
+-- Comprobar que los datos se agregaron de manera correcta (205,436 tuplas)
+SELECT * FROM cleaning.vehicle;
--- cleaning/raw_data_schema_creation.sql
+++ cleaning/raw_data_schema_creation.sql
@@ -1,32 +1,56 @@
+-- Se requiere de la extensión PostGIS para poder guardar datos de tipo POINT como coordenadas
+-- Pasos para la instalación:
+    -- 1) Abrir Stack Builder
+    -- 2) Seleccionar la versión de PostgreSQL que se esté utilizando
+    -- 3) En la sección de Categorías -> Spatial Extensions -> PostGIS 3.5 descargar la extensión
+    -- 4) Una vez terminada la instalación correr el sigueinte comando: CREATE EXTENSION postgis;
+
 DROP SCHEMA IF EXISTS raw CASCADE;
 CREATE SCHEMA IF NOT EXISTS raw;
 
 DROP TABLE IF EXISTS raw.vehicle_data;
 CREATE TABLE raw.vehicle_data (
-    vin VARCHAR(10),                    -- Ejemplo: JTMAB3FV3P
-    county varchar(50),                 -- Ejemplo: Kitsap
-    city varchar(50),                   -- Ejemplo: Seabeck
-    state varchar(50),                  -- Ejemplo: WA
-    postal_code VARCHAR(10),            -- Ejemplo: 98380
-    model_year smallint,                -- Ejemplo: 2023
-    make varchar(50),                   -- Ejemplo: TOYOTA
-    model varchar(50),                  -- Ejemplo: RAV4 PRIME
-    vehicle_type varchar(50),           -- Ejemplo: Plug-in Hybrid Electric Vehicle (PHEV)
-    CAFV varchar(100),                  -- Ejemplo: Clean Alternative Fuel Vehicle Eligible
-    range smallint,                     -- Ejemplo: 42
-    baseMSRP numeric(10,2),             -- Ejemplo: 0 (puede ser un valor decimal)
-    legislative_district varchar(4),    -- Ejemplo: 35
-    dol_vehicle_id BIGINT PRIMARY KEY , -- Ejemplo: 240684006
-    vehicle_location text,              -- Ejemplo: POINT (-122.8728334 47.5798304)
-    electric_utility text,              -- Ejemplo: PUGET SOUND ENERGY INC
-    census_tract varchar(20)            -- Ejemplo: 53035091301
+    vin VARCHAR(10),                        -- Ejemplo: JTMAB3FV3P
+    country VARCHAR(50),                    -- Ejemplo: Kitsap
+    city VARCHAR(50),                       -- Ejemplo: Seabeck
+    state VARCHAR(2),                       -- Ejemplo: WA
+    postal_code VARCHAR(10),                -- Ejemplo: 98380
+    model_year SMALLINT,                    -- Ejemplo: 2023
+    make VARCHAR(50),                       -- Ejemplo: TOYOTA
+    model VARCHAR(50),                      -- Ejemplo: RAV4 PRIME
+    vehicle_type VARCHAR(100),              -- Ejemplo: Plug-in Hybrid Electric Vehicle (PHEV)
+    CAFV VARCHAR(100),                      -- Ejemplo: Clean Alternative Fuel Vehicle Eligible
+    range SMALLINT DEFAULT 0,               -- Ejemplo: 42
+    baseMSRP NUMERIC(10,2),                 -- Ejemplo: 36,000.5
+    legislative_district VARCHAR(2),        -- Ejemplo: 35
+    dol_vehicle_id BIGINT PRIMARY KEY,      -- Ejemplo: 240684006
+    vehicle_location GEOMETRY(Point, 4326), -- Ejemplo: POINT (-122.8728334 47.5798304)
+    electric_utility TEXT,                  -- Ejemplo: PUGET SOUND ENERGY INC
+    census_tract VARCHAR(12)                -- Ejemplo: 53035091301
 );
 
 -- Ingresar los datos en MAC
 -- \copy raw.vehicle_data (vin, county, city, state, postal_code, model_year, make, model, vehicle_type, CAFV, range, baseMSRP, legislative_district, dol_vehicle_id, vehicle_location, electric_utility, census_tract) FROM '/Users/carlitos73/Documents/ITAM/Cuarto_Semestre/Bases_de_Datos/Electric_Vehicle_Population_Data.csv' WITH (FORMAT CSV, HEADER true, DELIMITER ',');
 
 -- Ingresar los datos en Windows
--- COPY raw.vehicle_data (vin, county, city, state, postal_code, model_year, make, model, vehicle_type, CAFV, range, baseMSRP, legislative_district, dol_vehicle_id, vehicle_location, electric_utility, census_tract) FROM 'D:/PABLO/ITAM/Materias 4 Semestre/Bases de Datos/Proyecto Final/Electric_Vehicle_Population_Data.csv' WITH (FORMAT CSV, HEADER true, DELIMITER ',');
+COPY raw.vehicle_data (vin, country, city, state, postal_code, model_year, make, model, vehicle_type, CAFV, range, baseMSRP, legislative_district, dol_vehicle_id, vehicle_location, electric_utility, census_tract) FROM 'D:/PABLO/ITAM/Materias 4 Semestre/Bases de Datos/Proyecto Final/Electric_Vehicle_Population_Data.csv' WITH (FORMAT CSV, HEADER true, DELIMITER ',');
 
--- Comprobar que los datos se ingresaron de manera correcta
+-- Por una razón desconocida el default no coloca las tuplas nulas con en valor por defecto
+-- Es necesario ejecutar las siguientes linear para que se realicen los cambios
+UPDATE raw.vehicle_data SET range = 0 WHERE range IS NULL;
+UPDATE raw.vehicle_data SET baseMSRP = 0.00 WHERE baseMSRP IS NULL;
+UPDATE raw.vehicle_data SET model = '-' WHERE model IS NULL;
+UPDATE raw.vehicle_data SET legislative_district = '-' WHERE legislative_district IS NULL;
+
+-- Debido a la incompletitud de los datos de las siguientes tres tuplas, pues no contienen información
+-- esencial como condado, ciudad, código postal, servicio de energía, código censal y ubicación del vehículo,
+-- decidimos como parte de la limpieza de la tabla eliminarlas
+DELETE FROM raw.vehicle_data WHERE raw.vehicle_data.dol_vehicle_id = 244582593;
+DELETE FROM raw.vehicle_data WHERE raw.vehicle_data.dol_vehicle_id = 159850029;
+DELETE FROM raw.vehicle_data WHERE raw.vehicle_data.dol_vehicle_id = 477613216;
+
+-- 205,436 tuplas deben de ser afectadas
 SELECT * FROM raw.vehicle_data;
+
+-- Para seleccionar un dato de tipo POINT como coordenadas de texto se debe hacer de la sigueinte manera:
+-- SELECT ST_AsText(vehicle_location) FROM raw.vehicle_data;
